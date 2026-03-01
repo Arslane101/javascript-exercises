c9276c52c0c14aa4072996551b8164d082f3e5f5
Arslane101 <rahalabdelhamid@gmail.com>
2026-03-01 16:49:25 +0100

    Exercices 9,1O,11 . Some test cases are needed

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temperature) {
+  var multiplier = Math.pow(10, 1 || 0);
+  value =   ((temperature-32)*(5/9))
+  return (Math.round(value * multiplier) / multiplier)
+  };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  var multiplier = Math.pow(10, 1 || 0);
+   value =   ((temperature*(9/5))+32)
+  return (Math.round(value * multiplier) / multiplier)
 };
 
 // Do not edit below this line
