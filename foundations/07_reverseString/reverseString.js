a377eed80a23c9832837605cf860c5245149124f
Arslane101 <rahalabdelhamid@gmail.com>
2026-03-01 15:44:04 +0100

    Exercices 6,7,8

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    array = string.split("")
+    result = []
+    for (i= array.length-1 ; i>=0; i--){
+        result.push(array[i])
+    }
+    return result.join("")
+
+
 
 };
 
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (let i=1 ; i<=arguments.length;i++){
+        position = array.indexOf(arguments[i])
+        while (position > -1 ){
+            array.splice(position,1)
+            position = array.indexOf(arguments[i])
+            }
+    }
+    return array
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
